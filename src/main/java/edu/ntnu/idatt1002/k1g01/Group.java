4bd402ef687415805a969d53a7b554dc69f62ebe
martdam <martdam@stud.ntnu.no>
2022-03-14 15:41:23 +0100

    Added dummy version of Group class

---
+++ src/main/java/edu/ntnu/idatt1002/k1g01/Group.java
@@ -0,0 +1,11 @@
+package edu.ntnu.idatt1002.k1g01;
+import java.util.ArrayList;
+
+/**
+ * Dummy class for easier coding
+ */
+public class Group {
+
+    public Group(ArrayList<Team> teams) {}
+
+}
