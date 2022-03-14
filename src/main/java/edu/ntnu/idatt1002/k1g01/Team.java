7d37cab1bb87b24ccfbfb8956ee48247f906f6db
Kristian Vaula <kristvje@stud.ntnu.no>
2022-03-14 14:01:11 +0100

    Added equals and hashcode to Team.java, with tests

--- src/main/java/edu/ntnu/idatt1002/k1g01/Team.java
+++ src/main/java/edu/ntnu/idatt1002/k1g01/Team.java
@@ -1,12 +1,15 @@
 package edu.ntnu.idatt1002.k1g01;
 
 
+import java.util.Objects;
+
 /**
  * Represents a team in the tournament
  */
 public class Team {
     // Name of the team
     private String name;
+
 
     /**
      * Initiates a new team
@@ -22,5 +25,28 @@ public class Team {
      */
     public String getName() {
         return name;
+    }
+
+    /**
+     * Two teams equal each other if
+     * they have the same name
+     * @param o The team we are comparing to
+     * @return True if teams have the same name
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Team team = (Team) o;
+        return Objects.equals(name, team.name);
+    }
+
+    /**
+     * Hashes name of team
+     * @return The hashcode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/test/java/edu/ntnu/idatt1002/k1g01/TeamTest.java
+++ src/test/java/edu/ntnu/idatt1002/k1g01/TeamTest.java
@@ -9,7 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TeamTest {
 
     @Nested
-    public class constructorTest{
+    @DisplayName("Tests the constructor(s)")
+    public class ConstructorTest{
 
         @Test
         @DisplayName("Initiates with correct name")
@@ -20,6 +21,45 @@ public class TeamTest {
 
             assertEquals(team.getName(),name);
         }
+    }
 
+    @Nested
+    @DisplayName("Equals and HashCode")
+    public class EqualsAndHashCode{
+
+        @Test
+        @DisplayName("Two teams with same name are equal")
+        public void TwoTeamsWithSameNameAreEqual(){
+            String testName = "TestName";
+            Team team1 = new Team(testName);
+            Team team2 = new Team(testName);
+
+            boolean equality = team1.equals(team2);
+
+            assertTrue(equality);
+        }
+
+        @Test
+        @DisplayName("Two teams with different name are not equal")
+        public void TwoTeamsWithDifferentNameAreNotEqual(){
+            String testName = "TestName";
+            String otherTestName = "otherTestName";
+            Team team1 = new Team(testName);
+            Team team2 = new Team(otherTestName);
+
+            boolean equality = team1.equals(team2);
+
+            assertFalse(equality);
+        }
+
+        @Test
+        @DisplayName("Two equal teams have the same hashcode")
+        public void TwoEqualTeamsHaveTheSameHashCode(){
+            String testName = "TestName";
+            Team team1 = new Team(testName);
+            Team team2 = new Team(testName);
+
+            assertEquals(team1.hashCode(),team2.hashCode());
+        }
     }
 }
