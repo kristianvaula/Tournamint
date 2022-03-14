e546273eef5a654c4b218e7abd7b1a1d8100272b
tniversen <thomasiversen01@gmail.com>
2022-03-14 15:48:47 +0100

    Started creating the Round Test class

---
+++ src/test/java/edu/ntnu/idatt1002/k1g01/RoundTest.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idatt1002.k1g01;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoundTest {
+
+    @Test
+    void getMatches() {
+        ArrayList<Match> matches = new ArrayList<Match>();
+        //matches.add(...)
+        assertEquals(..., getMatches());
+    }
+}
