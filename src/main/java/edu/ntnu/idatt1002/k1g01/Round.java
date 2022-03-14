9f5c5f9bcb9d67383cb24332f5bd45937d4b980b
tniversen <thomasiversen01@gmail.com>
2022-03-14 15:12:33 +0100

    Created the Round class

---
+++ src/main/java/edu/ntnu/idatt1002/k1g01/Round.java
@@ -0,0 +1,27 @@
+package edu.ntnu.idatt1002.k1g01;
+
+import java.util.ArrayList;
+
+/**
+ * Represents one round in the tournament
+ */
+public class Round {
+
+    private ArrayList<Match> matches; // All the current matches being played in the specific round
+
+    /**
+     * Initiates a round
+     * @param matches Matches in the round
+     */
+    public Round(ArrayList<Match> matches) {
+        this.matches = matches;
+    }
+
+    /**
+     * Get the matches in the specific round
+     * @return matches
+     */
+    public ArrayList<Match> getMatches() {
+        return matches;
+    }
+}
