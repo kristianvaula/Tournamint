b6aee9299a859a21088580786b7bfd274a8c66f9
martdam <martdam@stud.ntnu.no>
2022-03-20 18:23:54 +0100

    Added flesh to Group class and made accompanying test class. NEEDS MORE WORK! Constructor does not generate optimal number of rounds.

--- src/main/java/edu/ntnu/idatt1002/k1g01/Group.java
+++ src/main/java/edu/ntnu/idatt1002/k1g01/Group.java
@@ -1,11 +1,158 @@
 package edu.ntnu.idatt1002.k1g01;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import edu.ntnu.idatt1002.k1g01.matches.*;
 
-/**
- * Dummy class for easier coding
- */
 public class Group {
+    private final ArrayList<Team> teams;
+    private final ArrayList<Round> rounds;
+    private final ArrayList<Match> matches;
 
-    public Group(ArrayList<Team> teams) {}
 
+    /**
+     * Creates a group of teams. Every team will be paired up in a match against every other team.
+     * Matches will be grouped into rounds, so that every match in a round can be played concurrently.
+     * @param matchType Class of matches to generate. Example: PointMatch.class
+     * @param teams ArrayList of teams for the group.
+     * @throws IllegalArgumentException if < 2 teams, or any duplicate teams in input.
+     */
+    public Group( Class matchType, ArrayList<Team> teams) {
+        //validate input.
+        if (teams.size() < 2) {
+            throw new IllegalArgumentException("Attempted to create group with " + teams.size() + " teams. Minimum group size: 2!");
+        }
+        HashSet<Team> teamSet = new HashSet<>(teams);
+        if (teamSet.size() != teams.size())  {
+            throw new IllegalArgumentException("Attempted to create group with duplicate team entries!");
+        }
+
+        //Initialize class fields.
+        rounds = new ArrayList<>();
+        ArrayList<Match> tempMatches = new ArrayList<>();
+        matches = new ArrayList<>();
+        this.teams = teams;
+
+        //Find all unique unordered match pairs.
+        for (Team teamA : teams) {
+            for (Team teamB: teams) {
+                if (teamA != teamB) {
+                    ArrayList<Team> teamsForMatch = new ArrayList<>();
+                    teamsForMatch.add(teamA); teamsForMatch.add(teamB);
+                    Match tempMatch;
+                    if (matchType == PointMatch.class) {
+                        tempMatch = new PointMatch(teamsForMatch);
+                    }
+                    else if (matchType == TimeMatch.class) {
+                        tempMatch = new TimeMatch(teamsForMatch);
+                    }
+                    else {throw new ClassCastException("Error in Group constructor: Unknown Match type!"); }
+                    boolean unique = true;
+                    for (Match match : matches) {
+                        if (tempMatch.equals(match)) { unique = false; break; }
+                    }
+                    if (unique) { matches.add(tempMatch); tempMatches.add(tempMatch); }
+                }
+            }
+        }
+
+        //Divide match pairs into minimum number of rounds so that each team has <= 1 entry per round.
+        int groupRound = 1;
+        int[] matchCountPerTeam = new int[teams.size()];
+        int maxMatchCount = 0;
+        while (tempMatches.size() > 0) {
+            ArrayList<Match> matchesInRound = new ArrayList<>();
+            ArrayList<Team> teamsInRound = new ArrayList<>();
+            Team requiredTeam = null;
+            //First, check if any team has fewer matches so far than others.
+            for (int i = 0; i < teams.size(); i++) {
+                if (matchCountPerTeam[i] < maxMatchCount) {
+                    requiredTeam = teams.get(i);
+                }
+            }
+            //Add as many matches as possible to this round. Prioritize matches with teams that have fewer matches.
+            ArrayList<Match> doubleTempMatches = new ArrayList<>();
+            doubleTempMatches.addAll(tempMatches);
+            doubleTempMatches.addAll(tempMatches);
+            for (Match tempMatch : doubleTempMatches) {
+                boolean teamsFree = true;
+                for (Team team : tempMatch.getParticipants()) {
+                    if (teamsInRound.contains(team)) { teamsFree = false; break; }
+                }
+                if (teamsFree && (requiredTeam == null || tempMatch.getParticipants().contains(requiredTeam))) {
+                    requiredTeam = null;
+                    matchesInRound.add(tempMatch);
+                    teamsInRound.addAll(tempMatch.getParticipants());
+                    for (Team matchTeam: tempMatch.getParticipants()) {
+                        matchCountPerTeam[teams.indexOf(matchTeam)]++;
+                    }
+                }
+            }
+            //Update maxMatchCountPerTeam for the creation of next round.
+            for (int count : matchCountPerTeam) {
+                if (count > maxMatchCount) {
+                    maxMatchCount = count;
+                    break;
+                }
+            }
+            tempMatches.removeAll(matchesInRound);
+            rounds.add(new Round(matchesInRound, "groupRound_" + groupRound));
+            groupRound++;
+        }
+    }
+
+    /**
+     * Overloaded constructor. Does not require team list on the form of an ArrayList.
+     * Creates a group of teams. Every team will be paired up in a match against every other team.
+     * Matches will be grouped into rounds, so that every match in a round can be played concurrently.
+     * @param matchType Class of matches to generate. Example: PointMatch.class
+     * @param teams teams for the group.
+     * @throws IllegalArgumentException if < 2 teams, or any duplicate teams in input.
+     */
+    public Group( Class matchType, Team ...teams) {
+        this(matchType, new ArrayList<>(Arrays.asList(teams)));
+    }
+
+    //Dumb getters
+    public ArrayList<Team> getTeams() { return teams; }
+    public ArrayList<Round> getRounds() { return rounds; }
+    public ArrayList<Match> getMatches() { return matches; }
+
+    /**
+     * Finds the n best performing teams in this group.
+     * WARNING! Only accounts for single best team in each match, as this class currently only handles 2 teams per match.
+     * TODO: Handle draws better! Currently awards group victory to team with lowest list index.
+     * @param n Number of teams to fetch.
+     * @return ArrayList of top teams in descending order. Null if some matches are not finished.
+     * @throws NoSuchFieldException if not all matches in group are finished.
+     */
+    public ArrayList<Team> getTopTeams(int n) throws NoSuchFieldException {
+        if (n > teams.size()) {
+            throw new IndexOutOfBoundsException("Requested top "+n+" teams from group with only "+teams.size()+" teams!");
+        }
+        //Get number of wins per team.
+        int[] points = new int[teams.size()];
+        for (Match match : matches) {
+            if (! match.isFinished()) {
+                throw new NoSuchFieldException("Not all matches have finished");
+            }
+            Team winner = match.getWinners(1).get(0);
+            points[teams.indexOf(winner)]++;
+        }
+        //Assemble output ArrayList.
+        ArrayList<Team> output = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int highScore = Integer.MIN_VALUE + 1;
+            int bestTeamIndex = 0;
+            for (int x = 0; x < points.length; x++) {
+                if (points[x] > highScore) {
+                    highScore = points[x];
+                    bestTeamIndex = x;
+                }
+            }
+            output.add(teams.get(bestTeamIndex));
+            points[bestTeamIndex] = Integer.MIN_VALUE;
+        }
+        return output;
+    }
 }
---
+++ src/test/java/edu/ntnu/idatt1002/k1g01/matches/Grouptest.java
@@ -0,0 +1,177 @@
+package edu.ntnu.idatt1002.k1g01.matches;
+
+import edu.ntnu.idatt1002.k1g01.Group;
+import edu.ntnu.idatt1002.k1g01.Round;
+import edu.ntnu.idatt1002.k1g01.Team;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Grouptest {
+
+    private ArrayList<Team> generateTeams(int n){
+        ArrayList<Team> teams = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int roll = i % 6;
+            switch (roll) {
+                case 0: { teams.add(new Team("pingas_" + i/6 )); break; }
+                case 1: { teams.add(new Team("luigi_" + i/6 )); break; }
+                case 2: { teams.add(new Team("princess_" + i/6 )); break; }
+                case 3: { teams.add(new Team("maiboi_" + i/6 )); break; }
+                case 4: { teams.add(new Team("stinker_" + i/6 )); break; }
+                case 5: { teams.add(new Team("frog_" + i/6 )); break; }
+            }
+        }
+        return teams;
+    }
+
+    @Test
+    public void CanConstructWith_2() {
+        Team teamPingas = new Team("Pingas");
+        Team teamLuigi = new Team("Luigi");
+        Group group = new Group(PointMatch.class, teamPingas, teamLuigi);
+    }
+    @Test
+    public void CanConstructWith_3() {
+        Group group = new Group(PointMatch.class, generateTeams(3));
+    }
+    @Test
+    public void CanConstructWith_4() {
+        Group group = new Group(PointMatch.class, generateTeams(4));
+    }
+    @Test
+    public void CanConstructWith_5() {
+        Group group = new Group(PointMatch.class, generateTeams(5));
+    }
+    @Test
+    public void CanConstructWith_6() {
+        Group group = new Group(PointMatch.class, generateTeams(6));
+    }
+    @Test
+    public void CanConstructWith_10() {
+        Group group = new Group(PointMatch.class, generateTeams(10));
+    }
+    @Test
+    public void CanConstructWith_13() {
+        Group group = new Group(PointMatch.class, generateTeams(13));
+    }
+    @Test
+    public void CanConstructWith_17() {
+        Group group = new Group(PointMatch.class, generateTeams(17));
+    }
+
+    @Test
+    public void ConstructorTrowsCorrectExceptions() {
+        //Initialize teams
+        Team teamPingas = new Team("Pingas");
+        Team teamLuigi = new Team("Luigi");
+        //Perform tests
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Group group = new Group(PointMatch.class, teamPingas);
+        });
+        String expected = "Attempted to create group with " + 1 + " teams. Minimum group size: 2!";
+        String actual = exception.getMessage();
+        assertEquals(expected, actual);
+        exception = assertThrows(IllegalArgumentException.class, () -> {
+            Group group = new Group(PointMatch.class, teamPingas, teamPingas);
+        });
+        expected = "Attempted to create group with duplicate team entries!";
+        actual = exception.getMessage();
+        assertEquals(expected, actual);
+    }
+
+    /* TODO: Fix Group constructor to make groups of ideal size and not fail this test.
+    @Test
+    public void constructorGeneratesCorrectNumberOfRounds() {
+        Group group = new Group(PointMatch.class, generateTeams(2));
+        assertEquals(1, group.getRounds().size());
+        group = new Group(PointMatch.class, generateTeams(3));
+        assertEquals(3, group.getRounds().size());
+        group = new Group(PointMatch.class, generateTeams(4));
+        assertEquals(3, group.getRounds().size());
+        group = new Group(PointMatch.class, generateTeams(5));
+        assertEquals(5, group.getRounds().size());
+        group = new Group(PointMatch.class, generateTeams(6));
+        assertEquals(5, group.getRounds().size());
+    }
+    */
+
+    @Test
+    public void constructorGeneratesRoundsWithNoDuplicateTeams() {
+        for (int n = 2; n < 20; n++) {
+            Group group = new Group(PointMatch.class, generateTeams(n));
+            for (Round round : group.getRounds()) {
+                HashSet<Team> teamSet = new HashSet<>();
+                ArrayList<Team> teamList = new ArrayList<>();
+                for (Match match : round.getMatches()) {
+                    teamSet.addAll(match.getParticipants());
+                    teamList.addAll(match.getParticipants());
+                    assertEquals(teamSet.size(), teamList.size(), "Detected round with duplicate teams in group with "+n+" teams");
+                }
+            }
+        }
+    }
+
+    @Test
+    public void getTopTeamsTrowsCorrectExceptions() {
+        Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
+            Group group = new Group(PointMatch.class, generateTeams(4));
+            group.getTopTeams(5);
+        });
+        String expected = "Requested top "+5+" teams from group with only "+4+" teams!";
+        String actual = exception.getMessage();
+        assertEquals(expected, actual);
+        exception = assertThrows(NoSuchFieldException.class, () -> {
+            Group group = new Group(PointMatch.class, generateTeams(4));
+            group.getTopTeams(2);
+        });
+        expected = "Not all matches have finished";
+        actual = exception.getMessage();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getTopTeamsGetsCorrectTeams() throws NoSuchFieldException {
+        //Create teams
+        Team teamPingas = new Team("Pingas");
+        Team teamLuigi = new Team("Luigi");
+        Team teamPrincess = new Team("Princess");
+
+        //Pingas Wins
+        Group group = new Group(PointMatch.class, teamPingas, teamLuigi);
+        group.getMatches().get(0).setResult(teamPingas, "3");
+        group.getMatches().get(0).setResult(teamLuigi, "1");
+        assertEquals( teamPingas, group.getTopTeams(1).get(0));
+
+        //Luigu Wins
+        group = new Group(PointMatch.class, teamPingas, teamLuigi);
+        group.getMatches().get(0).setResult(teamLuigi, "3");
+        group.getMatches().get(0).setResult(teamPingas, "2");
+        assertEquals( teamLuigi, group.getTopTeams(1).get(0));
+
+        //Princess Wins, Pingas second Luigi last.
+        group = new Group(PointMatch.class, teamPingas, teamLuigi, teamPrincess);
+        for (Match match : group.getMatches()) {
+            if(match.getParticipants().contains(teamPrincess)) {
+                match.setResult(teamPrincess, "4");
+                for (Team team : match.getParticipants()) {
+                    if (team != teamPrincess) {
+                        match.setResult(team, "1");
+                    }
+                }
+            }
+            else {
+                match.setResult(teamPingas, "2");
+                match.setResult(teamLuigi, "0");
+            }
+        }
+    assertEquals( teamPrincess, group.getTopTeams(3).get(0));
+    assertEquals( teamPingas, group.getTopTeams(3).get(1));
+    assertEquals( teamLuigi, group.getTopTeams(3).get(2));
+    }
+}
