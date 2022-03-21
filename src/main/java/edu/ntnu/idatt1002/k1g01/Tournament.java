78c4fa12c6a8b45d9b922dc7ca581911c8c54ade
Kristian Vaula <kristvje@stud.ntnu.no>
2022-03-21 10:50:16 +0100

    Tournament.java now has groupStage and knockoutStage as attributes and initiates stages

--- src/main/java/edu/ntnu/idatt1002/k1g01/Tournament.java
+++ src/main/java/edu/ntnu/idatt1002/k1g01/Tournament.java
@@ -1,6 +1,7 @@
 package edu.ntnu.idatt1002.k1g01;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Represents a tournament.
@@ -15,6 +16,33 @@ public class Tournament {
     private String tournamentName;
     //All the teams competing in the tournament
     private ArrayList<Team> teams;
+    //Group stage of tournament
+    private GroupStage groupStage;
+    //Knockout stage of tournament;
+    private KnockoutStage knockoutStage;
+    //How many teams play each other in a match
+    private int teamsPerMatch;
+    //What types of matches they play (PointMatch // TimeMatch)
+    private String matchType;
+
+    /**
+     * Initiates a new tournament.
+     * <br>Takes a name, list of teams,
+     * and a knockoutstage as arguments.
+     * @param tournamentName Name of tournament
+     * @param teams Teams to be in tournament
+     */
+    public Tournament(String tournamentName, ArrayList<Team> teams,String matchType,int teamsPerMatch) throws IllegalArgumentException{
+        if(teams.size() < 2) {
+            throw new IllegalArgumentException(
+                    "Cannot create a tournament with less than two teams");
+        }
+        this.tournamentName = tournamentName;
+        this.teams = teams;
+        this.teamsPerMatch = teamsPerMatch;
+        this.matchType = matchType;
+        this.knockoutStage = new KnockoutStage(teams,teamsPerMatch,matchType);
+    }
 
     /**
      * Initiates a new tournament.
@@ -22,14 +50,16 @@ public class Tournament {
      * teams as arguments.
      * @param tournamentName Name of tournament
      * @param teams Teams to be in tournament
+     *
      */
-    public Tournament(String tournamentName, ArrayList<Team> teams) throws IllegalArgumentException{
-        if(teams.size() == 0) {
+    public Tournament(String tournamentName, ArrayList<Team> teams, String matchType,int teamsPerMatch,int teamsPerGroup,int advancingPerGroup) throws IllegalArgumentException{
+        if(teams.size() < 2) {
             throw new IllegalArgumentException(
                     "Cannot create a tournament with less than two teams");
         }
         this.tournamentName = tournamentName;
         this.teams = teams;
+        this.groupStage = new GroupStage(teams,advancingPerGroup,teamsPerGroup,matchType);
     }
 
     /**
@@ -38,6 +68,14 @@ public class Tournament {
      */
     public String getTournamentName() {
         return tournamentName;
+    }
+
+    /**
+     * Sets the TournamentName
+     * @param tournamentName String name
+     */
+    public void setTournamentName(String tournamentName) {
+        this.tournamentName = tournamentName;
     }
 
     /**
@@ -59,11 +97,43 @@ public class Tournament {
     }
 
     /**
+     * Gets groupStage
+     * @return the groupStage
+     */
+    public GroupStage getGroupStage() {
+        return groupStage;
+    }
+
+    /**
+     * Gets knockoutStage
+     * @return the knockoutStage
+     */
+    public KnockoutStage getKnockoutStage() {
+        return knockoutStage;
+    }
+
+    /**
+     * Updates the tournament.
+     * If the groupStage is finished and the
+     * knockoutStage has not yet been created,
+     * we initiate the knockoutStage. Else we
+     * just call for the knockoutStage update
+     * method();
+     * @throws NoSuchFieldException
+     */
+    public void updateTournament() throws NoSuchFieldException{
+        if(groupStage.isFinished() && knockoutStage == null){
+            knockoutStage = new KnockoutStage(groupStage.getWinnersFromGroups(),teamsPerMatch,matchType);
+        }else{
+            knockoutStage.updateKnockoutStage();
+        }
+    }
+
+    /**
      * Adds a team to the tournament
      * <br> Checks if team is already in
      * tournament before it adds it to the
      * tournament team list.
-     * @return True if team added to list.
      */
     public void addTeam(Team team) throws IllegalArgumentException{
         if(teams.contains(team)){
@@ -71,5 +141,15 @@ public class Tournament {
                     "You cannot add the same team more than once");
         }
         else teams.add(team);
+    }
+
+    /**
+     * removes a team to the tournament
+     * <br> Checks if team is already in
+     * tournament before it removes it to the
+     * tournament team list.
+     */
+    public void removeTeam(Team team){
+        teams.remove(team);
     }
 }
--- src/test/java/edu/ntnu/idatt1002/k1g01/TournamentTest.java
+++ src/test/java/edu/ntnu/idatt1002/k1g01/TournamentTest.java
@@ -1,5 +1,7 @@
 package edu.ntnu.idatt1002.k1g01;
 
+import edu.ntnu.idatt1002.k1g01.matches.PointMatch;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TournamentTest {
 
+    private Team team1,team2,team3, team4;
+    private ArrayList<Team> teamList1, teamList2, teamList3;
+    private KnockoutStage knockoutStage1, knockoutStage2;
+
+    @BeforeEach
+    void setUp() {
+        team1 = new Team("TeamName1");
+        team2 = new Team("TeamName2");
+        team3 = new Team("TeamName3");
+        team4 = new Team("TeamName4");
+
+        teamList1 = new ArrayList<>();
+        teamList1.add(team1);
+        teamList1.add(team2);
+
+        teamList2 = new ArrayList<>();
+        teamList2.add(team3);
+        teamList2.add(team4);
+
+        teamList3 = new ArrayList<>();
+        teamList3.add(team1);
+        teamList3.add(team2);
+        teamList3.add(team3);
+        teamList3.add(team4);
+    }
+
     @Nested
     @DisplayName("Tests the constructor(s)")
     public class ConstructorTest{
@@ -17,11 +45,9 @@ public class TournamentTest {
         @Test
         @DisplayName("Initiates a tournament")
         public void InitiatesATournament(){
-            ArrayList<Team> teamlist = new ArrayList<>();
-            teamlist.add(new Team("TestTeam"));
             String testName = "testName";
 
-            Tournament tournament = new Tournament(testName,teamlist);
+            Tournament tournament = new Tournament(testName,teamList3,"PointMatch",2,4,2);
 
             assertEquals(testName,tournament.getTournamentName());
         }
@@ -29,11 +55,11 @@ public class TournamentTest {
         @Test
         @DisplayName("Initiates a tournament with empty team list throws exception")
         public void InitiatesATournamentWithEmptyTeamList(){
-            ArrayList<Team> teamlist = new ArrayList<>();
+            ArrayList<Team> teamList = new ArrayList<>();
             String testName = "testName";
 
             try{
-                Tournament tournament = new Tournament(testName,teamlist);
+                Tournament tournament = new Tournament(testName,teamList,"PointMatch",2,4,2);
                 fail();
             }catch(IllegalArgumentException e){
                 assertEquals(e.getClass(),new IllegalArgumentException().getClass());
@@ -48,37 +74,31 @@ public class TournamentTest {
         @Test
         @DisplayName("getTeams() gets list of teams")
         public void GetTeamsGetsCorrectTeamList(){
-            ArrayList<Team> teamList = new ArrayList<>();
-            Team team = new Team("TestTeam");
-            teamList.add(team);
-            Tournament tournament = new Tournament("testName",teamList);
+            Tournament tournament = new Tournament("testName",teamList3,"PointMatch",2,4,2);
 
             ArrayList<Team> checkList = tournament.getTeams();
 
-            assertEquals(checkList.get(0),team);
+            assertEquals(teamList3, checkList);
         }
 
         @Test
         @DisplayName("addTeam() with a new team")
         public void AddTeamWithNewTeam(){
-            ArrayList<Team> teamList = new ArrayList<>();
-            teamList.add(new Team("TestTeam"));
-            Tournament tournament = new Tournament("testName",teamList);
-
+            Tournament tournament = new Tournament("testName",teamList3,"PointMatch",2,4,2);
 
             Team newTeam = new Team("NewTeam");
             tournament.addTeam(newTeam);
 
-            assertTrue(tournament.getTeams().size() == 2);
+            assertEquals(newTeam,tournament.getTeams().get(tournament.getNumberOfTeams()-1));
         }
 
         @Test
         @DisplayName("addTeam() with the same team throws IllegalArgumentException")
         public void AddTeamWithSameTeam(){
-            ArrayList<Team> teamList = new ArrayList<>();
             Team testTeam = new Team("TestTeam");
-            teamList.add(testTeam);
-            Tournament tournament = new Tournament("testName",teamList);
+            teamList3.add(testTeam);
+
+            Tournament tournament = new Tournament("testName",teamList3,"PointMatch",2,4,2);
 
             try{
                 tournament.addTeam(testTeam);
@@ -91,12 +111,10 @@ public class TournamentTest {
         @Test
         @DisplayName("getNumberOfTeam() returns correct amount of teams")
         public void GetCorrectNumberOfTeams(){
-            ArrayList<Team> teamList = new ArrayList<>();
-            teamList.add(new Team("TestTeam"));
-            Tournament tournament = new Tournament("testName",teamList);
+            Tournament tournament = new Tournament("testName",teamList3,"PointMatch",2,4,2);
 
             int tournamentSizeBefore = tournament.getNumberOfTeams();
-            teamList.add(new Team("NewTeam"));
+            teamList3.add(new Team("NewTeam"));
             int tournamentSizeAfter = tournament.getNumberOfTeams();
 
             assertTrue(tournamentSizeBefore + 1 == tournamentSizeAfter);
