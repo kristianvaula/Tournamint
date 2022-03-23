cb129d4c08b1e81b995644034d7b7e7cd1c325c0
Kristian Vaula <kristvje@stud.ntnu.no>
2022-03-23 08:42:26 +0100

    Added AdministrateTournament.fxml and AdministrateTournamentController.java

---
+++ src/main/java/edu/ntnu/idatt1002/k1g01/controller/AdministrateTournamentController.java
@@ -0,0 +1,14 @@
+package edu.ntnu.idatt1002.k1g01.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdministrateTournamentController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
