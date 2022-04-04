0d259947b1ef9e0131d78ee75c2f0e845d200d08
Espen Justad <espenjus@stud.ntnu.no>
2022-04-04 16:16:50 +0200

    Added BracketMatchBlockController, BracketMatchBlock.fxml, BracketRoundContainer.fxml and BracketRoundContainerController

--- src/main/java/edu/ntnu/idatt1002/k1g01/controller/DisplayModeController.java
+++ src/main/java/edu/ntnu/idatt1002/k1g01/controller/DisplayModeController.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt1002.k1g01.controller;
 
+import edu.ntnu.idatt1002.k1g01.dao.TournamentDAO;
 import edu.ntnu.idatt1002.k1g01.model.Round;
 import edu.ntnu.idatt1002.k1g01.model.Team;
 import edu.ntnu.idatt1002.k1g01.model.Tournament;
@@ -11,6 +12,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,46 +23,53 @@ public class DisplayModeController implements Initializable {
     //The nested controller for the menuBar
     @FXML private TopMenuBarController topMenuBarController;
 
-    @FXML
-    private Text nameField;
-
+    //The tournament variables
     private Tournament tournament;
+    private TournamentDAO tournamentDAO;
 
-    //labels for 1/8 finals
-    @FXML private Label slot1, slot2, slot3, slot4,slot5, slot6, slot7, slot8, slot9, slot10, slot11, slot12, slot13, slot14, slot15, slot16;
-    @FXML private Label points1, points2, points3, points4, points5, points6, points7, points8, points9, points10, points11, points12, points13, points14, points15, points16;
+    @FXML
+    private Text tournamentNameOutput;
 
-    //Labels for the quarterfinals
-    @FXML private Label quarterfinalSlot1, quarterfinalSlot2, quarterfinalSlot3, quarterfinalSlot4, quarterfinalSlot5, quarterfinalSlot6, quarterfinalSlot7, quarterfinalSlot8;
-    @FXML private Label quarterfinalPoints1, quarterfinalPoints2, quarterfinalPoints3, quarterfinalPoints4, quarterfinalPoints5, quarterfinalPoints6, quarterfinalPoints7, quarterfinalPoints8;
 
-    //Labels for the semifinals
-    @FXML private Label semifinalSlot1, semifinalSlot2, semifinalSlot3, semifinalSlot4;
-    @FXML private Label semifinalPoints1, semifinalPoints2, semifinalPoints3, semifinalPoints4;
 
-    //Labels for the final
-    @FXML private Label finalSlot1, finalSlot2;
-    @FXML private Label finalPoints1, finalPoints2;
-
-    public ArrayList<Label> arrangeLabels() {
-        ArrayList<Label> slots = new ArrayList<>(); slots.add(quarterfinalPoints1); slots.add(quarterfinalPoints2);
-        slots.add(quarterfinalSlot3); slots.add(quarterfinalSlot4); slots.add(quarterfinalSlot5); slots.add(quarterfinalSlot7);
-        slots.add(quarterfinalSlot8);
-        return slots;
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        nameField.setText(tournament.getTournamentName());
     }
 
+    /**
+     * Starts session for administrating a tournament with TournamentDAO.
+     * Makes file containing tournament accessible so that it can be easily updated frequently.
+     * TODO give user reassuring feedback whenever tournament file is updated.
+     * TODO better user feedback for errors.
+     * @param tournamentDAO DAO for tournament object. Must be non-null.
+     */
     @FXML
-    public void initData(Tournament tournament) {
-        this.tournament = tournament;
+    public void initData(TournamentDAO tournamentDAO){
+        this.tournamentDAO = tournamentDAO;
+        topMenuBarController.setTournamentDAO(tournamentDAO); //Pass DAO pointer to nested controller.
+        try {
+            this.tournament = tournamentDAO.load();
+        }
+        catch (IOException ioException) {
+            System.out.println("Error in initData: " + ioException.getMessage());
+            //TODO handle exception if loading somehow fails. Should not be possible at this point.
+        }
+        tournamentNameOutput.setText(tournament.getTournamentName());
     }
 
+    public void fillRoundOf16() {
+        ArrayList<Match> matchesInRoundOf16 = new ArrayList<>();
+        for(Round round : this.tournament.getKnockoutStage().getRounds()){
+            if(round.getRoundName().equals("ROUND OF 16")){
+                matchesInRoundOf16.addAll(round.getMatches());
+            }
+        }
 
 
+
+
+    }
 
 
 
