1f80819a4309e5dc698fb6a8cc2cbb0c557fb2f3
Espen Justad <espenjus@stud.ntnu.no>
2022-04-04 10:47:15 +0200

    Changes to KnockoutStage part of DisplayMode.fxml

--- src/main/java/edu/ntnu/idatt1002/k1g01/controller/DisplayModeController.java
+++ src/main/java/edu/ntnu/idatt1002/k1g01/controller/DisplayModeController.java
@@ -1,16 +1,29 @@
 package edu.ntnu.idatt1002.k1g01.controller;
 
+import edu.ntnu.idatt1002.k1g01.model.Round;
+import edu.ntnu.idatt1002.k1g01.model.Team;
+import edu.ntnu.idatt1002.k1g01.model.Tournament;
+import edu.ntnu.idatt1002.k1g01.model.matches.Match;
+import edu.ntnu.idatt1002.k1g01.model.stages.KnockoutStage;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DisplayModeController implements Initializable {
 
     //The nested controller for the menuBar
     @FXML private TopMenuBarController topMenuBarController;
+
+    @FXML
+    private Text nameField;
+
+    private Tournament tournament;
 
     //TextFields for the quarterfinals
     @FXML
@@ -25,7 +38,30 @@ public class DisplayModeController implements Initializable {
     @FXML private TextField finalSlot1, finalSlot2;
     @FXML private TextField finalPoints1, finalPoints2;
 
+    public ArrayList<TextField> arrangeTextFields() {
+        ArrayList<TextField> slots = new ArrayList<>(); slots.add(quarterfinalPoints1); slots.add(quarterfinalPoints2);
+        slots.add(quarterfinalSlot3); slots.add(quarterfinalSlot4); slots.add(quarterfinalSlot5); slots.add(quarterfinalSlot7);
+        slots.add(quarterfinalSlot8);
+        return slots;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        nameField.setText(tournament.getTournamentName());
     }
+
+    @FXML
+    public void initData(Tournament tournament) {
+        this.tournament = tournament;
+    }
+
+
+
+
+
+
+
+
+
+
 }
