4df793fa9334a6d7216a5f4fa903749f92d5bcb7
Espen Justad <espenjus@stud.ntnu.no>
2022-04-06 12:09:10 +0200

    Added methods to DisplayModeController and BracketRoundContainerController

--- src/main/java/edu/ntnu/idatt1002/k1g01/controller/BracketRoundContainerController.java
+++ src/main/java/edu/ntnu/idatt1002/k1g01/controller/BracketRoundContainerController.java
@@ -1,17 +1,45 @@
 package edu.ntnu.idatt1002.k1g01.controller;
 
+import edu.ntnu.idatt1002.k1g01.model.Round;
+import edu.ntnu.idatt1002.k1g01.model.matches.Match;
+import edu.ntnu.idatt1002.k1g01.model.stages.KnockoutStage;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class BracketRoundContainerController implements Initializable {
 
-
+    private final static String DLM = File.separator;
+    private VBox BracketRound;
+    private ArrayList<BracketMatchBlockController> matchControllers;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    /**
+     * method for adding MatchBlocks to roundContainers depending on how many matches in each round
+     * @param round round of the knockout stage
+     * @throws IOException
+     */
+    public void setMatchesInRoundContainers(Round round) throws IOException {
+        for (Match match : round.getMatches()) {
+            FXMLLoader loader = new FXMLLoader();
+            VBox roundContainer = loader.load(Objects.requireNonNull(getClass().getResource("edu" + DLM + "ntnu" + DLM + "idatt1002" + DLM + "k1g01" + DLM + "view" + DLM + "BracketMatchBlock.fxml")));
+            BracketRound.getChildren().add(roundContainer);
+            BracketMatchBlockController controller = loader.getController();
+            matchControllers.add(controller);
+        }
+    }
+
+
 
 
 }
--- src/main/java/edu/ntnu/idatt1002/k1g01/controller/DisplayModeController.java
+++ src/main/java/edu/ntnu/idatt1002/k1g01/controller/DisplayModeController.java
@@ -7,25 +7,33 @@ import edu.ntnu.idatt1002.k1g01.model.Tournament;
 import edu.ntnu.idatt1002.k1g01.model.matches.Match;
 import edu.ntnu.idatt1002.k1g01.model.stages.KnockoutStage;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class DisplayModeController implements Initializable {
 
+    private final static String DLM = File.separator;
     //The nested controller for the menuBar
     @FXML private TopMenuBarController topMenuBarController;
 
     //The tournament variables
     private Tournament tournament;
     private TournamentDAO tournamentDAO;
+    @FXML private HBox outerHbox;
+    private ArrayList<BracketRoundContainerController> roundControllers;
 
     @FXML
     private Text tournamentNameOutput;
@@ -45,7 +53,7 @@ public class DisplayModeController implements Initializable {
      * @param tournamentDAO DAO for tournament object. Must be non-null.
      */
     @FXML
-    public void initData(TournamentDAO tournamentDAO){
+    public void initData(TournamentDAO tournamentDAO) throws IOException {
         this.tournamentDAO = tournamentDAO;
         topMenuBarController.setTournamentDAO(tournamentDAO); //Pass DAO pointer to nested controller.
         try {
@@ -56,25 +64,42 @@ public class DisplayModeController implements Initializable {
             //TODO handle exception if loading somehow fails. Should not be possible at this point.
         }
         tournamentNameOutput.setText(tournament.getTournamentName());
+        if (!(tournament.getKnockoutStage() == null && tournament.getKnockoutStage().getRounds().isEmpty())) {
+            setBracketRoundContainers();
+        }
     }
 
-    public void fillRoundOf16() {
-        ArrayList<Match> matchesInRoundOf16 = new ArrayList<>();
-        for(Round round : this.tournament.getKnockoutStage().getRounds()){
-            if(round.getRoundName().equals("ROUND OF 16")){
-                matchesInRoundOf16.addAll(round.getMatches());
+    /**
+     * method for setting round bracket containers in the display mode for knockout stage based on
+     * number of rounds in the knockout stage.
+     * @throws IOException
+     */
+    public void setBracketRoundContainers() throws IOException {
+       int numberOfRounds = this.tournament.getKnockoutStage().getRounds().size();
+        if (numberOfRounds == 0) throw new IllegalArgumentException("Number of rounds in knockout stage is zero");
+        else if (numberOfRounds > 4) {
+            for (int i = numberOfRounds - 4; i < numberOfRounds; i++) {
+                addBracketRoundContainer(this.tournament.getKnockoutStage().getRounds().get(i));
+            }
+        } else {
+            for (int i = 0; i < numberOfRounds; i++) {
+                addBracketRoundContainer(this.tournament.getKnockoutStage().getRounds().get(i));
             }
         }
-
-
-
-
     }
 
-
-
-
-
+    /**
+     * method for adding a BracketRoundContainer to the display section.
+     * @throws IOException
+     */
+    public void addBracketRoundContainer(Round round) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        VBox roundContainer = loader.load(Objects.requireNonNull(getClass().getResource("edu" + DLM + "ntnu" + DLM + "idatt1002" + DLM + "k1g01" + DLM + "view" + DLM + "BracketRoundContainer.fxml")));
+        outerHbox.getChildren().add(roundContainer);
+        BracketRoundContainerController controller = loader.getController();
+        controller.setMatchesInRoundContainers(round);
+        roundControllers.add(controller);
+    }
 
 
 
