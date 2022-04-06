2efc256d58333bf360391e7234fca500e935a539
Kristian Vaula <kristvje@stud.ntnu.no>
2022-04-06 14:29:07 +0200

    Incoming changes

---
+++ src/main/java/edu/ntnu/idatt1002/k1g01/controller/BracketMatchBlockController.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idatt1002.k1g01.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BracketMatchBlockController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+}
---
+++ src/main/java/edu/ntnu/idatt1002/k1g01/controller/BracketRoundContainerController.java
@@ -0,0 +1,50 @@
+package edu.ntnu.idatt1002.k1g01.controller;
+
+import edu.ntnu.idatt1002.k1g01.model.Round;
+import edu.ntnu.idatt1002.k1g01.model.matches.Match;
+import edu.ntnu.idatt1002.k1g01.model.stages.KnockoutStage;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class BracketRoundContainerController implements Initializable {
+
+    private final static String DLM = File.separator;
+    @FXML private VBox BracketRoundContainer;
+    private ArrayList<BracketMatchBlockController> matchControllers = new ArrayList<>();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("***initializing BracketRoundContainerController");
+        System.out.println("***     BracketRoundContainer = " + this.BracketRoundContainer);
+    }
+
+    /**
+     * method for adding MatchBlocks to roundContainers depending on how many matches in each round
+     * @param round round of the knockout stage
+     * @throws IOException
+     */
+    public void setMatchesInRoundContainers(Round round) throws IOException {
+        for (Match match : round.getMatches()) {
+            FXMLLoader loader = new FXMLLoader();
+            HBox matchBlock = loader.load(Objects
+                    .requireNonNull(getClass().getResource("../view/BracketMatchBlock.fxml"))
+                    .openStream());
+            BracketRoundContainer.getChildren().add(matchBlock);
+            BracketMatchBlockController controller = loader.getController();
+            matchControllers.add(controller);
+        }
+    }
+
+
+
+
+}
