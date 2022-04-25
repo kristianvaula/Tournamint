35fbe4e5f609146fa478bad78c65a383bd2a6cd8
Kristian Vaula <kristvje@stud.ntnu.no>
2022-04-25 17:25:56 +0200

    AdministrateTournamentController.java can open selected match using Enter key

--- src/main/java/edu/ntnu/idatt1002/k1g01/controller/AdministrateTournamentController.java
+++ src/main/java/edu/ntnu/idatt1002/k1g01/controller/AdministrateTournamentController.java
@@ -7,14 +7,18 @@ import edu.ntnu.idatt1002.k1g01.model.matches.Match;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
@@ -24,10 +28,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Objects;
-import java.util.ResourceBundle;
+import java.util.*;
 
 /**
  * Controls administration of tournament
@@ -82,9 +83,16 @@ public class AdministrateTournamentController implements Initializable {
         infoColumn.setCellValueFactory(new PropertyValueFactory<>("matchInfo"));
         matchTable.setEditable(false);
 
+        matchTable.setOnKeyReleased(event -> {
+            if(event.getCode() == KeyCode.ENTER){
+                if(!matchTable.getSelectionModel().isEmpty()){
+                    Match match = matchTable.getSelectionModel().getSelectedItem();
+                    enterResultEvent(event,match);
+                }
+            }
+        });
         matchTable.setRowFactory(tableView -> {
             TableRow<Match> row = new TableRow<>();
-
             row.setOnMouseClicked(event ->{
                 stopClock = true;
                 if (!row.isEmpty() && event.getButton()== MouseButton.PRIMARY
@@ -232,7 +240,7 @@ public class AdministrateTournamentController implements Initializable {
      * send the match instance.
      */
     @FXML
-    public void enterResultEvent(MouseEvent event,Match match){
+    public void enterResultEvent(Event event, Match match){
         try {
             FXMLLoader loader = new FXMLLoader();
             System.out.println(tournament.getMatchType());
